349d2f9da391a1a1c2ab2abc20820413ddfadc5f
Donguk Kim <ehddnr73@naver.com>
2023-03-07 14:32:47 +0900

    Domain module version upgraded

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/ehddnr3473/Domain.git",
-            .upToNextMajor(from: "1.0.0")
+            .upToNextMajor(from: "1.1.0")
         )
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
